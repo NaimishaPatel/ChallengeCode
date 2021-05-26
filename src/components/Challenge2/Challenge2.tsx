d299221aa08071f458147276de68c986e32c2189
Chris Lee <chris.lee@rate.com>
2021-05-26 09:40:59 -0700

    add DoubleClick and FizzBuzz challenges

--- src/components/Challenge2/Challenge2.tsx
+++ src/components/Challenge2/Challenge2.tsx
@@ -1,9 +1,58 @@
 import styles from "./Challenge2.module.scss";
 
+export const Solution: React.FC = () => {
+  return (
+    <>
+      <button>CLICK ME</button>
+      <div>Last Click Time: TODO</div>
+    </>
+  );
+};
+
 export const Challenge2: React.FC = () => {
-  return <div className={styles.Challenge2}>
-    <h2>Challenge 2</h2>
-    <p>Place your work below this element.</p>
-    <p>Place your work below this element.</p>
-  </div>
+  return (
+    <div className={styles.Challenge2}>
+      <h2>Challenge 2</h2>
+      <h3>Double Click</h3>
+      <div>
+        This challenge exercises your React component development skills.
+        <ul>
+          <li>
+            Clicking the button below should update the div with the time of the
+            last click. The time can be displayed in any format.
+          </li>
+          <li>
+            If the user clicks the button then it should pop up a notification
+            saying that the button was clicked.
+          </li>
+          <li>The notification should disappear after 3 seconds.</li>
+          <li>The notification should be made reusable.</li>
+          <li>
+            Implement your solution entirely within this Challenge folder
+            (Challenge2).
+          </li>
+        </ul>
+      </div>
+      <h3>Discussion</h3>
+      <div>
+        <ul>
+          <li>
+            What are the considerations for making this notification component
+            reusable?
+          </li>
+        </ul>
+      </div>
+      <h3>Further Enhancement (after initial implementation)</h3>
+      <div>
+        <ul>
+          <li>
+            Change the single click to instead react only a double-click within
+            500ms.
+          </li>
+        </ul>
+      </div>
+      <h3>Implementation</h3>
+      <Solution />
+    </div>
+  );
 };
--- src/components/Challenge3/Challenge3.tsx
+++ src/components/Challenge3/Challenge3.tsx
@@ -1,9 +1,45 @@
 import styles from "./Challenge3.module.scss";
 
+export const Solution: React.FC = () => {
+  return (
+    <>
+      <button>CLICK ME</button>
+      <div>Counter: TODO</div>
+      <div>fizzbuzz</div>
+    </>
+  );
+};
+
 export const Challenge3: React.FC = () => {
-  return <div className={styles.Challenge3}>
-    <h2>Challenge 3</h2>
-    <p>Place your work below this element.</p>
-    <p>Place your work below this element.</p>
-  </div>
+  return (
+    <div className={styles.Challenge3}>
+      <h2>Challenge 3</h2>
+      <h3>FizzBuzz</h3>
+      <div>
+        This challenge exercises your React component development skills.
+        <ul>
+          <li>
+            Your Solution component needs to be modified to take 2 props: "fizz"
+            and "buzz", both are numbers. If you can use TypeScript,{" "}
+          </li>
+          <li>Clicking the button below should update the counter +1.</li>
+          <li>
+            If the counter is divisible by your prop "fizz", the component
+            should also output the word "fizz".
+          </li>
+          <li>
+            If the counter is divisible by your prop "buzz", the component
+            should also output the word "buzz".
+          </li>
+          <li>
+            If the counter is divisible by both X and Y, the component should
+            output "fizzbuzz" together.
+          </li>
+          <li>Implement your solution entirely within this file.</li>
+        </ul>
+      </div>
+      <h3>Implementation</h3>
+      <Solution />
+    </div>
+  );
 };
